457c217a56537db5e7706494b75251fe217f1e7e
Mai Truong <truong.mt89@gmail.com>
2021-08-11 19:33:08 -0400

    updated mood

--- src/App.js
+++ src/App.js
@@ -1,38 +1,67 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Title from './Components/Title';
 import EntryList from './Components/EntryList';
+import Entries from './Components/EntryList';
 import axios from "axios";
 import Calendar from './Components/Calendar';
 import Moods from './Components/Moods';
 import './App.css'
 
-// function App() {
-//   const axios = require('axios')
-//   const BASE_URL = "http://localhost:5000/mood-catcher";
-//   useEffect(() => {
-//     axios.get(`${BASE_URL}/Title`)
-//     .then((response) => console.log(response))
-//     .catch((err) => console.log(err));
-//   }
-//   )};
+const App = () => {
+  const [entries, setEntries] = useState([])
+  const[color, setColor] = useState('#efb6b2')
+  const axios = require('axios')
+  const BASE_URL = "http://localhost:5000/mood-catcher";
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+      .then((response) => {
+        setEntries(response.data)
+      })
+      .catch((err) => console.log(err));
+  },
+    []
+  )
 
-  function App() {
-    const[color, setColor] = useState('#efb6b2')
-    const BASE_URL = "http://localhost:5000/mood-catcher";
-    return (
-      <div className="App">
-        <Title />
-        <div id = "appContainer">
-          <div id = "calendarContainer">
-            <Calendar tileColor = {color}/>
-          </div>
-          <div id = 'moodContainer'>
-            <Moods colorAction = {setColor}/>
-          </div>
+  const removeEntry = index => {
+    setEntries(
+      entries.filter((entry, i) => {
+        return i !== index
+      })
+    )
+  }
+  return (
+    <div className="App">
+      <Title />
+      <div id="appContainer">
+        <div id="calendarContainer">
+          <Calendar tileColor={color} />
+        </div>
+        <div id='moodContainer'>
+          <Moods colorAction={setColor} />
+          <Entries entryData={entries} removeEntry={removeEntry} />
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  )
+};
+
+// function App() {
+//   const[color, setColor] = useState('#efb6b2')
+//   const BASE_URL = "http://localhost:5000/mood-catcher";
+//   return (
+//     <div className="App">
+//       <Title />
+//       <div id = "appContainer">
+//         <div id = "calendarContainer">
+//           <Calendar tileColor = {color}/>
+//         </div>
+//         <div id = 'moodContainer'>
+//           <Moods colorAction = {setColor}/>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 
 export default App;
